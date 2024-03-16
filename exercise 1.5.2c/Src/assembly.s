911c93072c0a01cd7fe443b205a30705b4edd71f
GuoJiashu <jacob1365826588@163.com>
2024-03-16 19:31:05 +1100

    exercise 4c

---
+++ exercise 1.5.2c/Src/assembly.s
@@ -0,0 +1,85 @@
+.syntax unified
+.thumb
+
+.global main
+
+#include "initialise.s"
+
+.data
+@ define variables
+
+
+.align
+
+@ Define a string
+tx_string: .asciz "hello?\r\n"
+tx_length: .byte 10
+
+
+.text
+@ define text
+
+
+main:
+
+	BL initialise_power          @ Call function to initialise power
+	BL enable_peripheral_clocks  @ Call function to enable peripheral clocks
+	BL enable_uart               @ Call function to enable UART
+	BL change_clock_speed        @ Call function to change clock speed
+
+	B program_loop               @ Branch to program loop
+
+program_loop:
+
+	LDR R0, =GPIOA      @ Load address of GPIOA into R0
+	LDRB R1, [R0, #0x10]@ Load byte from memory address GPIOA + 0x10 into R1
+	CMP R1, #255        @ Compare R1 with 255
+	BEQ tx_loop         @ Branch to tx_loop if equal
+
+	B program_loop      @ Branch back to program_loop if not equal
+
+tx_loop:
+
+	LDR R0, =USART1     @ Load address of USART1 into R0
+
+	LDR R3, =tx_string  @ Load address of tx_string into R3
+	LDR R4, =tx_length  @ Load address of tx_length into R4
+
+	LDR R4, [R4]        @ Load value from memory address stored in R4 into R4
+
+
+tx_uart:
+
+	LDR R1, [R0, USART_ISR]   @ Load USART ISR register into R1
+	ANDS R1, 1 << UART_TXE    @ Perform bitwise AND operation to check UART_TXE flag
+
+	BEQ tx_uart               @ Branch back to tx_uart if UART_TXE flag is not set
+
+	LDRB R5, [R3], #1         @ Load byte from memory address pointed to by R3 into R5 and increment R3
+	STRB R5, [R0, USART_TDR]  @ Store byte from R5 into USART transmit data register
+
+	SUBS R4, #1          @ Subtract 1 from R4 (tx_length)
+
+	                     @ Compare R5 with ASCII code for '?'
+	CMP R5, #'?'
+	BEQ end_transmission @ Branch to end_transmission if equal
+
+	BGT tx_uart          @ Branch to tx_uart if R5 is greater than '?'
+
+	BL delay_loop        @ Call delay_loop subroutine
+
+	B tx_loop            @ Branch back to tx_loop
+
+end_transmission:
+
+	BL delay_loop        @ Call delay_loop subroutine
+	B program_loop       @ Branch back to program_loop
+
+delay_loop:
+	LDR R9, =0xfffff     @ Load maximum delay value into R9
+
+delay_inner:
+
+	SUBS R9, #1          @subtract 1 from R9
+	BGT delay_inner      @ Branch back to delay_inner if R9 is greater than 1
+	BX LR                @ Return from subroutine
---
+++ exercise 1.5.2c/Src/initialise.s
@@ -0,0 +1,70 @@
+.syntax unified
+.thumb
+
+#include "definitions.s"
+
+
+
+enable_peripheral_clocks:
+
+	LDR R0, =RCC
+
+	LDR R1, [R0, #AHBENR]
+	ORR R1, 1 << GPIOE_ENABLE | 1 << GPIOD_ENABLE | 1 << GPIOC_ENABLE | 1 << GPIOB_ENABLE | 1 << GPIOA_ENABLE  @ enable GPIO
+	STR R1, [R0, #AHBENR]
+
+	BX LR
+
+enable_uart:
+
+	LDR R0, =GPIOC
+
+	LDR R1, =0x77
+	STRB R1, [R0, AFRL + 2]
+
+	LDR R1, [R0, GPIO_MODER]
+	ORR R1, 0xA00
+	STR R1, [R0, GPIO_MODER]
+
+	LDR R1, [R0, GPIO_OSPEEDR]
+	ORR R1, 0xF00
+	STR R1, [R0, GPIO_OSPEEDR]
+
+	LDR R0, =RCC
+	LDR R1, [R0, #APB2ENR]
+	ORR R1, 1 << UART_EN
+	STR R1, [R0, #APB2ENR]
+
+	MOV R1, #0x8C
+	LDR R0, =USART1
+	STRH R1, [R0, #USART_BRR]
+
+	LDR R0, =USART1
+	LDR R1, [R0, #USART_CR1]
+	ORR R1, 1 << UART_TE | 1 << UART_RE | 1 << UART_UE
+
+	STR R1, [R0, #USART_CR1]
+
+	BX LR
+
+initialise_power:
+
+	LDR R0, =RCC
+
+	LDR R1, [R0, #APB1ENR]
+	ORR R1, 1 << PWREN
+	STR R1, [R0, #APB1ENR]
+
+	LDR R1, [R0, #APB2ENR]
+	ORR R1, 1 << SYSCFGEN
+	STR R1, [R0, #APB2ENR]
+
+	BX LR
+
+change_clock_speed:
+
+	LDR R0, =RCC
+	LDR R1, [R0, #RCC_CR]
+	LDR R2, =1 << HSEBYP | 1 << HSEON
+	ORR R1, R2
+	STR R1, [R0, #RCC_CR]
