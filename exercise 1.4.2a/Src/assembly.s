449d278f8ab35f28cf80b5056f9ffc287aa3a444
MarcoJara <162090860+MarcoJara@users.noreply.github.com>
2024-03-20 10:03:12 +1100

    Update assembly.s

--- exercise 1.4.2a/Src/assembly.s
+++ exercise 1.4.2a/Src/assembly.s
@@ -22,9 +22,9 @@ main:
 	BL initialise_discovery_board
 
 	@ store the current light pattern (binary mask) in R4
-	LDR R4, = 0b00110011 @ load a pattern for the set of LEDs (every second one is on)
+	LDR R4, = 0b00110011 		@ Toad a pattern for the set of LEDs (every second one is on)
 
 
-	LDR R0, =GPIOE  @ load the address of the GPIOE register into R0
-	STRB R4, [R0, #ODR + 1]   @ store this to the second byte of the ODR (bits 8-15)
-	EOR R4, #0xFF	@ toggle all of the bits in the byte (1->0 0->1)
+	LDR R0, =GPIOE  		@ Load the address of the GPIOE register into R0
+	STRB R4, [R0, #ODR + 1]   	@ Store this to the second byte of the ODR (bits 8-15)
+	EOR R4, #0xFF			@ Toggle all of the bits in the byte (1->0 0->1)
