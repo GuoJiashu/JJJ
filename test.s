0ff8e825fe2d04bad907c6c98fb27729de8738a7
Jacksonfu24566 <160695086+Jacksonfu24566@users.noreply.github.com>
2024-03-20 09:02:22 +0800

    Update test.s

--- test.s
+++ test.s
@@ -5,6 +5,7 @@
 
 .data
 prompt: .asciz "AA" @ Define a string
+alphabets: .asciz "abcdefghijklmnopqrstuvwxyz"
 substitutionTable: .asciz "zyxwvutsrqponmlkjihgfedcba" @ Define a substitution table for encoding/decoding
 
 .text
